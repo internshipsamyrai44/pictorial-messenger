8c54174f767f61c9aef909fea13cf4f7f57eeac6
dyv21 <dyv21.dev@gmail.com>
2025-06-07 06:30:06 +0300

    chore(#222): add module federation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'pictorial_messenger',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './DirectWidget': './src/DirectWidget.tsx'
+      },
+      shared: {
+        react: {
+          requiredVersion: '^18.3.1',
+        },
+        'react-dom': {
+          requiredVersion: '^18.3.1',
+        }
+      }
+    })
+  ],
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+    rollupOptions: {
+      external: ['react', 'react-dom']
+    }
+  },
+  server: {
+    port: 4173
+  },
+  preview: {
+    port: 4173
+  }
+});
